aff36a570b4fafa32b2861ebf463430bf80b6062
Hamlet <67255886+HamletGal@users.noreply.github.com>
2021-03-05 21:31:56 +0400

    add send message input and button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import {useState} from "react";
 import './App.css';
 
 function App() {
+  const [input,setInput] = useState('');
+  const [messages,setMessages] = useState(['hello','hi','whats up']);
+
+  console.log(input);
+
+  const sendMessage = (event) => {
+    setMessages([...messages,input]);
+    setInput('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Hello Clever Programmers</h1>
+     <input value={input} onChange={(event)=>setInput(event.target.value)} />
+     <button onClick={sendMessage}>Send Messages</button>
+
+     {
+       messages.map((message) => (
+         <p>{message}</p>
+       ))
+     }
     </div>
   );
 }
